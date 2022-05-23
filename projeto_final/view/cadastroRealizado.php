16243b1f5a463e2ac2174ccc96212c504112172f
GabrielVithor <gabrielvithorcv@hotmail.com>
2022-05-23 02:22:01 -0300

    view-1.2

---
+++ projeto_final/view/cadastroRealizado.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/ca5c96c2d2.js" crossorigin="anonymous" defer></script>
+    <title>Home</title>
+    <style>
+        .w3-sidebar {
+            width: 120px;
+        }
+
+        * {
+            font-family: "Montserrat", sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+<button name="btnCadRealizado" class="w3-button w3-block w3-margin w3-blue w3-cell w3-
+roundlarge w3-display-middle" style="width: 90%;max-width:600px;">Cadastro Realizado Com Sucesso!</button>
+</body>
+</html>
